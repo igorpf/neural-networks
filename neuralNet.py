3f1f746526843bcd0e34aeef84c71d187e423e01
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2017-10-16 22:24:04 -0200

    Some jibber jabber

--- neuralNet.py
+++ neuralNet.py
@@ -4,9 +4,40 @@
 # Created by igor on 12/10/2017
 import numpy as np
 
-# Z na verdade é - sum(input*weight, 0 a n) - b (bias)
-def sigmoid(z):
-    return 1 / (1+np.exp(-z))
+class neuralNet():
+    """docstring for neuralNet"""
+    def __init__(self, neurons=[1]): 
+        """Constructor:
+        args:
+            neurons(list of ints):  list representing the size of each layer. The number
+                of layers is the size of the list (of course)
+        """       
+        #Don't forget to create bias neuron!!
+        self.layers = [np.random.rand(1,n)[0] for n in neurons] #create a list of lists with random weights
+
+
+"""
+Possibility: 
+    Net: 
+        List<Layer>
+            List<Neurons>
+                List<Integer> weights 
+
+"""
+        
+
+
+def sigmoid():
+    """ Returns a dict containing the sigmoid function and its derivative
+
+    Usage example: sigmoid()['fn'](10)
+    """
+    return {
+        'fn': lambda z: 1 / (1+np.exp(-z)),
+        'der': lambda z: z*(1-z)
+    }
 
 if __name__ == '__main__':
-    print sigmoid(-10)
+    print sigmoid()['fn'](-10)
+    l = [4,3,2]
+    print [np.random.rand(1,n)[0] for n in l]
