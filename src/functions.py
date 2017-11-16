ae0792ca562be541b6ba80721cd6734f3aada7c9
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2017-11-16 20:28:02 -0200

    Changing column and adding relu function

--- src/functions.py
+++ src/functions.py
@@ -10,6 +10,11 @@ sigmoid = {
     'der': lambda z: z*(1-z)
 }
 
+relu = {
+    'fn': lambda z:z if z>0 else 0,
+    'der': lambda z: 1 if z>0 else 0
+}
+
 #Error functions
 errorMeanSq = {
     'fn': lambda out, ans: 0.5 * np.power(out-ans,2),
